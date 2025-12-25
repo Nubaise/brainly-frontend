84decac64ca3e134f26a8449ff6c0920abb29f9d
Nubaise <113296164+Nubaise@users.noreply.github.com>
2025-12-26 00:52:25 +0530

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { Dashboard } from "./pages/dashboard";
-import { Signup } from "./pages/signup";
 import { Signin } from "./pages/signin";
+import { Signup } from "./pages/signup";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <Signin />
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
 }
+
 
 export default App;
