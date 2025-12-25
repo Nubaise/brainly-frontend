9e050419cd315fd21f7f27503dfc6e3d6505dcb4
Nubaise <113296164+Nubaise@users.noreply.github.com>
2025-12-25 19:28:57 +0530

    Card component

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,18 @@ import { ShareIcon } from './icons/Shareicon'
 function App() {
 
   return (
-    <>
-      <Button variant="primary" startIcon={<PlusIcon />} text="Add content" />
-      <Button variant="secondary" startIcon={<ShareIcon />} text="Share brain" />
-      <Card />
-    </>
+    <div className="p-4">
+      <div className="flex justify-end gap-4">
+        <Button variant="primary" startIcon={<PlusIcon />} text="Add content" />
+        <Button variant="secondary" startIcon={<ShareIcon />} text="Share brain" />
+      </div>
+      
+      <div className='flex gap-4 mt-4'>
+        <Card type='twitter' link="https://x.com/kirat_tw/status/2004140818991665323" title='First tweet'/>
+        <Card type='youtube' link="https://www.youtube.com/watch?v=S3whb2XT47Q" title='Awesome video'/>
+        
+      </div>
+    </div>
   )
 }
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,27 @@
 import { ShareIcon } from '../icons/Shareicon'
 
-export function Card() {
+interface CardProps {
+    title : string;
+    link : string;
+    type: "twitter" | "youtube" | "linkedin";
+}
+
+export function Card( { title, link, type } : CardProps ) {
     return <div>
-        <div className="max-w-72 border-gray-200 border-2 rounded-md shadow-md p-4 bg-white">
+        <div className="min-w-72 max-w-72 min-h-48 border-gray-200 border-2 rounded-md shadow-md p-4 bg-white">
             <div className="flex justify-between">
                 <div className="flex items-center gap-2 text-md">
                     <div className="text-gray-500">
                         <ShareIcon />
                     </div>
-                    Project ideas
+                    {title}
                 </div>
                 <div className="flex items-center">
                     <div className="pr-2 text-gray-500">
-                        <ShareIcon />
+                        <a href={link} target="_blank" >
+                            <ShareIcon />
+                        </a>
+                        
                     </div>
                     <div className="text-gray-500">
                         <ShareIcon />
@@ -20,12 +29,12 @@ export function Card() {
                 </div>
             </div>
             <div className="pt-4">
-                {/*<iframe className="w-full" src="https://www.youtube.com/embed/uAHW2LUyDEk?si=9ni9AFqBOOCsKBtC" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>*/}
-                {/*<iframe className="w-full" src="https://www.linkedin.com/embed/feed/update/urn:li:share:7409317359225761792?collapsed=1" height="669" width="504" frameBorder="0" allowFullScreen title="Embedded post"></iframe>*/}
+                { type === "youtube" && <iframe className="w-full" src={link.replace("watch", "embed").replace("?v=","/")} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>}
+                { type === "linkedin" && <iframe className="w-full" src={link} height="669" width="504" frameBorder="0" allowFullScreen title="Embedded post"></iframe>}
 
-                <blockquote className="twitter-tweet">
-                    <a href="https://twitter.com/username/status/807811447862468608"></a> 
-                </blockquote>
+                { type === "twitter" && <blockquote className="twitter-tweet">
+                    <a href={link.replace("x.com","twitter.com")}></a> 
+                </blockquote>}
 
             </div>
         </div>
